b916f02b00a4c4f04958ce01d393eaaecd80e14d
Tetsuya Shinone <info@semnil.com>
2014-04-26 23:06:14 +0900

    add some functions.

--- README.md
+++ README.md
@@ -7,5 +7,28 @@ Command line tool for the MSX-39(Pocket Miku) with the OS X.
 usages
 ------
 
+1) binary from stdin
+
     $ echo -en "\x90\x3c\x64" | nsx39i
     $ echo -en "\x90\x3c\0" | nsx39i
+
+
+2) binary from stdin
+
+    $ nsx39i -s some_file_path
+
+
+3) hex string in parameter
+
+    $ nsx39i -S "90 3c 64"
+    $ nsx39i -S "903c00"
+
+
+4) hex string from stdin(interactive)
+
+    $ nsx39i -S
+    > 903c64
+    > 90 3c 00
+    > q
+    bye.
+    $ 
--- nsx39i/main.m
+++ nsx39i/main.m
@@ -9,7 +9,8 @@
 #import <Foundation/Foundation.h>
 #import <CoreMIDI/CoreMIDI.h>
 
-#include "stdio.h"
+#include <stdio.h>
+#include <unistd.h>
 
 
 #define DEVICE_NAME CFSTR("NSX-39 ")
@@ -78,19 +79,117 @@ void sendNote(MIDIEndpointRef endPoint, Byte *message, UInt16 length)
     MIDISend(getOutPutPort(), endPoint, &packetList);
 }
 
-int main(int argc, const char **argv)
+int main(int argc, char **argv)
 {
+    int result, mode = 0;
+    unsigned int param_buf;
+    Byte read_data;
+    char read_buf[3], *read_param;
+    Byte send_data[256];
+    UInt16 len = 0, buf_ptr = 0;
+    FILE *fp = stdin;
+    
     CFStringRef endPointName = DEVICE_NAME;
     MIDIEndpointRef endPoint = getEndpointWithDisplayName(endPointName);
     
-    Byte buf[256];
-    UInt16 len = 0;
-    while (fscanf(stdin, "%c", &buf[len], NULL) != EOF) {
-        len++;
-    };
-    sendNote(endPoint, buf, len);
+    if (endPoint == 0) {
+        fprintf(stdout, "%s not found.\n", CFStringGetCStringPtr(DEVICE_NAME, kCFStringEncodingASCII));
+        return -1;
+    }
     
+    while ((result = getopt(argc, argv, "s:S")) != -1) {
+        switch (result) {
+            case 's':
+                // binary data read from a file
+                fp = fopen(optarg, "rb");
+                if (fp == NULL) {
+                    fprintf(stdout, "file cannot open.\n");
+                    return -2;
+                }
+                break;
+                
+            case 'S':
+                // convert to binary from the hex string
+                if (optind < argc && argv[optind][0] != '\0') {
+                    read_param = argv[optind];
+                    
+                    // convert to send data from a parameter string
+                    for (int i = 0;i < strlen(read_param);) {
+                        // skip white space
+                        while (i < strlen(read_param) && read_param[i] == ' ')
+                            i++;
+                        
+                        // from string to binary
+                        read_buf[buf_ptr++] = read_param[i++];
+                        if (buf_ptr == 2) {
+                            send_data[len++] = strtol(read_buf, NULL, 16);
+                            buf_ptr = 0;
+                        }
+                        
+                        // skip white space
+                        while (i < strlen(read_param) && read_param[i] == ' ')
+                            i++;
+                    }
+                    goto SEND_DATA;
+                } else {
+                    // real time converting and sending
+                    mode = 1;
+                    // print a prompt
+                    fprintf(stdout, "> ");
+                }
+                break;
+                
+            case '?':
+                fprintf(stdout, "unknown parameter.\n");
+            default:
+                goto MIDI_ENDPOINT_DISPOSE;
+        }
+    }
+    
+    while (fp != NULL && fscanf(fp, "%c", &read_data, NULL) != EOF) {
+        switch (mode) {
+            case 1:
+                // convert to send data from the hex string of stdin
+                if (read_data == 'q' || read_data == 'Q') {
+                    // exit from the interactive mode
+                    fprintf(stdout, "bye.\n");
+                    goto MIDI_ENDPOINT_DISPOSE;
+                } else if (read_data != '\n' && read_data != ' ' && read_data != '\t') {
+                    read_buf[buf_ptr++] = read_data;
+                    read_buf[buf_ptr] = '\0';
+                    if (buf_ptr == 2) {
+                        send_data[len++] = strtol(read_buf, NULL, 16);
+                        buf_ptr = 0;
+                    }
+                } else if (read_data == '\n') {
+                    // send data of one line
+                    sendNote(endPoint, send_data, len);
+                    buf_ptr = 0;
+                    len = 0;
+                    // print a prompt
+                    fprintf(stdout, "> ");
+                }
+                break;
+                
+            default:
+                // send data read from stdin
+                send_data[len] = read_data;
+                len++;
+                if (len >= sizeof(send_data))
+                    goto SEND_DATA;
+                break;
+        }
+    };
+    
+SEND_DATA:
+    if (send_data[0] != 0)
+        sendNote(endPoint, send_data, len);
+    
+MIDI_ENDPOINT_DISPOSE:
     MIDIEndpointDispose(endPoint);
+    
+    if (fp != NULL && fp != stdin)
+        fclose(fp);
     
     return 0;
 }
